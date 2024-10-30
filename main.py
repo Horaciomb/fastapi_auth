2a3190f893f1adc14d3668fc89d6f2db32a5088c
Horacio Molina Barroso <horaciomolinab0@gmail.com>
2024-10-30 11:44:05 -0400

    agregando autenticacion inicial

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from fastapi import FastAPI, Form
-from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.exceptions import HTTPException
 
 app=FastAPI()
 oauth2_scheme=OAuth2PasswordBearer(tokenUrl="token")
 
-app.post('/token')
-def login(username=Form(), password=Form()):
-    return "token"
+users={
+    "universal":{"username":"universal", "email":"horaciomolinab0@gmail.com", "password":"password"}
+}
+
+@app.post("/token")
+async def login(form_data:Annotated[OAuth2PasswordRequestForm,Depends()]):
+    user = users.get(form_data.username)
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect username")
+    return user
 
 
