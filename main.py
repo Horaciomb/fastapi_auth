f3696ce2f6831c76f4c04c4d0ff6f8030090ba1d
Horacio Molina Barroso <horaciomolinab0@gmail.com>
2024-10-30 10:36:39 -0400

    Primer commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, Form
+from fastapi.security import OAuth2PasswordBearer
+
+app=FastAPI()
+oauth2_scheme=OAuth2PasswordBearer(tokenUrl="token")
+
+app.post('/token')
+def login(username=Form(), password=Form()):
+    return "token"
+
+
